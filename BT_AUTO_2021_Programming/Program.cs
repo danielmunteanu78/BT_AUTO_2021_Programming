e0eb1fdfdd7f0b611b9a54153db4bed16d2f6bcd
Daniel Munteanu <florindaniel.munteanu@gmail.com>
2021-12-06 10:35:23 +0200

    Update on the program.

--- BT_AUTO_2021_Programming/Program.cs
+++ BT_AUTO_2021_Programming/Program.cs
@@ -7,6 +7,9 @@ namespace BT_AUTO_2021_Programming
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ana are pere");
+            Console.WriteLine(100 + 21);
+            Console.WriteLine("100 + 21");
         }
     }
 }
